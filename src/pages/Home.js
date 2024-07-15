eabbc63e12cee8bbeff1e594d20689afed2c741d
Lepha <lepharamchiary@gmail.com>
2024-07-15 11:05:24 +0530

    home page complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div className='mt-60'>Home</div>
-  )
+    return (
+        <div className='mt-40 md:mt-28 flex h-full w-full '>
+            <div className='md:flex md:flex-row flex flex-col justify-center items-center max-w-2xl mx-auto gap-3 p-6'>
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                </div>
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
